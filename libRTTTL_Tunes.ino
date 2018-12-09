f8db3bffcd413b72fd8082a4300e4853e3c7f1d0
tonzik <39554893+tonzik@users.noreply.github.com>
2018-12-09 10:30:30 +0200

    Update libRTTTL_Tunes.ino

--- libRTTTL_Tunes.ino
+++ libRTTTL_Tunes.ino
@@ -1,6 +1,7 @@
 #include <libRTTTL_Tunes.h>
 
 // Määritetään buzzerin pinni Arduinoille
+// Define buzzer pin
 libRTTTL_Tunes player(A0);
 
 void setup() {
@@ -9,10 +10,8 @@ void setup() {
 
 void loop() {
 
-  // Soitetaan kappaleet 1-7 vuorotellen, 10 sekunnin välein
-  // Voidaan myös käyttää toistamaan tietty kappale kommennolla esim. player.playTuneNumber(1)
-  for(int i = 1; i < 8; i++) {
-    player.playTuneNumber(i);
-    delay(10000);
-  }
+  // Soittaa kappaleen 10 sekunnin välein
+  // Repeat the song every 10 seconds
+  player.playTuneNumber(1);
+  delay(10000);
 }
